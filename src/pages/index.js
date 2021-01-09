6cbe56478720f9a9443ee0798730e79dec8c06b0
Elie Mukau Ambel <s109059@ap.be>
2021-01-09 23:57:04 +0100

    vervolg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,51 @@
 import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
+import { useStaticQuery, graphql } from "gatsby"
 
-const IndexPage = () => (
+const IndexPage = () => {
+  const data = useStaticQuery(graphql
+    `
+    query{
+      wpcontent{
+      page(id: "home", idType: URI){
+        homeMeta{
+          homePageDescription
+          homePageHeaderDescription
+          homePageHeaderTitle
+          homePageHeaderPicture{
+            imageFile{
+              childImageSharp{
+                fluid(quality: 100){
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+          homePageFeaturedArtists{
+            ... on WPGraphql_Artist{
+              id
+              artistMeta{
+                artistName
+                firstName
+                lastName
+                profile{
+                  altText
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  `);
+  console.log(data);
+return(
   <Layout>
-    <SEO title="Home" />
-  </Layout>
-)
+  <SEO title="Home" />
+</Layout>
+)}
+
 
 export default IndexPage
