2028b87c366aa9788f8b8a9b32834603ab9e3041
Elie Mukau Ambel <s109059@ap.be>
2021-01-09 23:04:41 +0100

    les 6

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,44 @@
-import { Link } from "gatsby"
-import {useStaticQuery, graphql} from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import PropTypes from "prop-types"
+import Menu from "./Menu"
+import { HeaderWrapper, Image } from "./headerStyles/headerStyles"
 
-const Header = ({ siteTitle }) => (
-  <div>Header</div>
-)
+const Header = ({ siteTitle }) => {
+  const {
+    logo,
+    wpcontent: { menuItems },
+  } = useStaticQuery(graphql`
+    query {
+      logo: file(relativePath: { eq: "logo.png" }) {
+        childImageSharp {
+          fixed(quality: 100, width: 200) {
+            ...GatsbyImageSharpFixed_withWebp
+          }
+        }
+      }
+      wpcontent {
+        menuItems {
+          edges {
+            node {
+              label
+              path
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <HeaderWrapper>
+      <Link to="/">
+        <Image alt="logo artist agency" fixed={logo.childImageSharp.fixed} />
+      </Link>
+      <Menu menuItems={menuItems.edges} />
+    </HeaderWrapper>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
